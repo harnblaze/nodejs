a55080584c1656f7325a205f5c668ae1fc2dafaa
Dmitriy Emelyanov <harnblaze@gmail.com>
2023-04-04 12:21:53 +0300

    start node

---
+++ notes.controller.js
@@ -0,0 +1,37 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+
+  notes.push(note);
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.green.inverse("Note was added"));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, "utf-8");
+  const result = JSON.parse(notes);
+  return Array.isArray(result) ? result : [];
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+  console.log(chalk.bgBlue("Here is the list of notes:"));
+  notes.forEach((note) => {
+    console.log(chalk.blue(note.title));
+  });
+}
+
+module.exports = {
+  addNote,
+  printNotes,
+};
